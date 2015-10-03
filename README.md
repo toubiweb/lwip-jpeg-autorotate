011cedf98d868cc0b12606a27ab267cb7546978f
Toub <n.toublanc@gmail.com>
2015-10-03 18:33:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# lwip-jpeg-autorotate [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Read exif data and auto rotate jpeg images lwip library.
+
+
+lwip-jpeg-autorate try to read jpeg exif information and auto-rotate / auto-flip image according to "Orientation" tag.
+
+It fails silently if EXIF data is not fount or image is not jpeg.
+
+## Install
+
+```sh
+$ npm install --save lwip-jpeg-autorotate
+```
+
+## Usage
+
+```js
+var lwipJpegAutorotate = require('lwip-jpeg-autorotate');
+
+lwipJpegAutorotate('Rainbow');
+```
+
+## Development
+
+### Logs
+
+Simple logs messages are managed using https://github.com/visionmedia/debug
+
+To display them, use env variable :
+
+DEBUG=lwip-jpeg-autorotate
+
+### Generator
+
+Project generated from https://github.com/yeoman/generator-node.
+
+### Tests
+
+Tests are run by [http://mochajs.org](mocha) test framework and [http://chaijs.com](chai) assertion library.
+
+Tests images are provided by https://github.com/recurser/exif-orientation-examples under MIT licence.
+
+To run tests + coverage:
+
+    npm test
+    
+or
+    
+    gulp
+
+To run tests only
+
+    gulp test
+
+To enable logs:
+
+    DEBUG=lwip-jpeg-autorotate gulp test
+
+## License
+
+Apache-2.0 © [Nicolas Toublanc]()
+
+
+[npm-image]: https://badge.fury.io/js/lwip-jpeg-autorotate.svg
+[npm-url]: https://npmjs.org/package/lwip-jpeg-autorotate
+[travis-image]: https://travis-ci.org/toubiweb/lwip-jpeg-autorotate.svg?branch=master
+[travis-url]: https://travis-ci.org/toubiweb/lwip-jpeg-autorotate
+[daviddm-image]: https://david-dm.org/toubiweb/lwip-jpeg-autorotate.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/toubiweb/lwip-jpeg-autorotate
+[coveralls-image]: https://coveralls.io/repos/toubiweb/lwip-jpeg-autorotate/badge.svg
+[coveralls-url]: https://coveralls.io/r/toubiweb/lwip-jpeg-autorotate
---
+++ test/index.js
@@ -0,0 +1,140 @@
+import chai from 'chai';
+import path from 'path';
+import Q from 'q';
+import lwipJpegAutorotate from '../lib';
+
+var expect = chai.expect;
+var assert = chai.assert
+
+describe('lwip-jpeg-autorotate', function () {
+
+    it('Do not rotate PNG', function () {
+        
+        return doTest(1, '.png').then(function (res) {
+
+            expect(res.operations).to.deep.equal({});
+        });
+    });
+
+    it('Do not rotate JPG with Orientation 1', function () {
+
+        return doTest(1, '.jpg').then(function (res) {
+
+            expect(res.operations).to.deep.equal({});
+        });
+    });
+
+     it('Do not rotate JPG with Orientation 2', function () {
+
+        return doTest(2, '.jpg').then(function (res) {
+
+            expect(res.operations).to.deep.equal({
+                mirror: {
+                    axes: 'x'
+                }
+            });
+        });
+    });
+
+    
+    it('Do not rotate JPG with Orientation 3', function () {
+
+        return doTest(3, '.jpg').then(function (res) {
+
+            expect(res.operations).to.deep.equal({
+                rotate: {
+                    angle: 180
+                }
+            });
+        });
+    });
+
+    it('Do not rotate JPG with Orientation 4', function () {
+
+        return doTest(4, '.jpg').then(function (res) {
+
+            expect(res.operations).to.deep.equal({
+                mirror: {
+                    axes: 'y'
+                }
+            });
+        });
+    });
+
+    it('Do not rotate JPG with Orientation 5', function () {
+
+        return doTest(5, '.jpg').then(function (res) {
+
+            expect(res.operations).to.deep.equal({
+                mirror: {
+                    axes: 'x'
+                },
+                rotate: {
+                    angle: 90
+                }
+            });
+        });
+    });
+
+  it('Do not rotate JPG with Orientation 6', function () {
+
+        return doTest(6, '.jpg').then(function (res) {
+
+            expect(res.operations).to.deep.equal({
+                rotate: {
+                    angle: 90
+                }
+            });
+        });
+    });
+
+    it('Do not rotate JPG with Orientation 7', function () {
+
+        return doTest(7, '.jpg').then(function (res) {
+
+            expect(res.operations).to.deep.equal({
+                mirror: {
+                    axes: 'x'
+                },
+                rotate: {
+                    angle: 270
+                }
+            });
+        });
+    });
+    
+    it('Do not rotate JPG with Orientation 8', function () {
+
+        return doTest(8, '.jpg').then(function (res) {
+
+            expect(res.operations).to.deep.equal({
+                rotate: {
+                    angle: 270
+                }
+            });
+        });
+    });
+
+});
+
+
+function doTest(index, ext) {
+
+    var inputpath = path.resolve(__dirname, './images/Landscape_' + index + ext);
+    var outputpath = path.resolve(__dirname, './images/Landscape_' + index + '.output' + ext);
+
+    var promise = lwipJpegAutorotate.autorotate(inputpath, outputpath);
+
+    promise.then(function (res) {
+
+        expect(res).to.not.be.undefined;
+        expect(res.operations).to.not.be.undefined;
+
+    }, function (err) {
+        assert.fail('Unexpected error during auto-rotation.');
+    });
+
+    promise.done();
+
+    return promise;
+}
