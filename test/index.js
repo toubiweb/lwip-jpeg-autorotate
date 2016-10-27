975e33a7d4bcfd00476694100e8406303eb25e69
Toub <n.toublanc@gmail.com>
2016-10-27 15:14:31 +0000

    Upgrade to lwip 0.0.9

--- test/index.js
+++ test/index.js
@@ -1,3 +1,7 @@
+// test.js
+import test from 'ava'
+
+
 import chai from 'chai';
 import path from 'path';
 import Q from 'q';
@@ -6,138 +10,115 @@ import lwipJpegAutorotate from '../lib';
 var expect = chai.expect;
 var assert = chai.assert
 
-describe('lwip-jpeg-autorotate', function () {
+test('Do not rotate PNG', async t => {
+  return doTest(1, '.png').then(res => {
+    t.deepEqual(res.operations, {});
+  });
+})
 
-    it('Do not rotate PNG', function () {
-        
-        // this test is slower
-        this.timeout(5000);
-        
-        return doTest(1, '.png').then(function (res) {
+test('Do not rotate JPG with Orientation 1', async t => {
+  return doTest(1, '.jpg').then(res => {
+    t.deepEqual(res.operations, {});
+  });
+})
 
-            expect(res.operations).to.deep.equal({});
-        });
+test('Rotate JPG with Orientation 2', async t => {
+  return doTest(2, '.jpg').then(res => {
+    t.deepEqual(res.operations, {
+      mirror: {
+        axes: 'x'
+      }
     });
+  });
+})
 
-    it('Do not rotate JPG with Orientation 1', function () {
-
-        return doTest(1, '.jpg').then(function (res) {
-
-            expect(res.operations).to.deep.equal({});
-        });
+test('Rotate JPG with Orientation 3', async t => {
+  return doTest(3, '.jpg').then(res => {
+    t.deepEqual(res.operations, {
+      rotate: {
+        angle: 180
+      }
     });
+  });
+})
 
-     it('Do not rotate JPG with Orientation 2', function () {
-
-        return doTest(2, '.jpg').then(function (res) {
-
-            expect(res.operations).to.deep.equal({
-                mirror: {
-                    axes: 'x'
-                }
-            });
-        });
+test('Rotate JPG with Orientation 4', async t => {
+  return doTest(4, '.jpg').then(res => {
+    t.deepEqual(res.operations, {
+      mirror: {
+        axes: 'y'
+      }
     });
+  });
+})
 
-    
-    it('Do not rotate JPG with Orientation 3', function () {
-
-        return doTest(3, '.jpg').then(function (res) {
-
-            expect(res.operations).to.deep.equal({
-                rotate: {
-                    angle: 180
-                }
-            });
-        });
+test('Rotate JPG with Orientation 5', async t => {
+  return doTest(5, '.jpg').then(res => {
+    t.deepEqual(res.operations, {
+      mirror: {
+        axes: 'x'
+      },
+      rotate: {
+        angle: 90
+      }
     });
+  });
+})
 
-    it('Do not rotate JPG with Orientation 4', function () {
-
-        return doTest(4, '.jpg').then(function (res) {
-
-            expect(res.operations).to.deep.equal({
-                mirror: {
-                    axes: 'y'
-                }
-            });
-        });
+test('Rotate JPG with Orientation 6', async t => {
+  return doTest(6, '.jpg').then(res => {
+    t.deepEqual(res.operations, {
+      rotate: {
+        angle: 90
+      }
     });
+  });
+})
 
-    it('Do not rotate JPG with Orientation 5', function () {
-
-        return doTest(5, '.jpg').then(function (res) {
-
-            expect(res.operations).to.deep.equal({
-                mirror: {
-                    axes: 'x'
-                },
-                rotate: {
-                    angle: 90
-                }
-            });
-        });
+test('Rotate JPG with Orientation 7', async t => {
+  return doTest(7, '.jpg').then(res => {
+    t.deepEqual(res.operations, {
+      mirror: {
+        axes: 'x'
+      },
+      rotate: {
+        angle: 270
+      }
     });
+  });
+})
 
-  it('Do not rotate JPG with Orientation 6', function () {
-
-        return doTest(6, '.jpg').then(function (res) {
-
-            expect(res.operations).to.deep.equal({
-                rotate: {
-                    angle: 90
-                }
-            });
-        });
+test('Rotate JPG with Orientation 8', async t => {
+  return doTest(8, '.jpg').then(res => {
+    t.deepEqual(res.operations, {
+      rotate: {
+        angle: 270
+      }
     });
-
-    it('Do not rotate JPG with Orientation 7', function () {
-
-        return doTest(7, '.jpg').then(function (res) {
-
-            expect(res.operations).to.deep.equal({
-                mirror: {
-                    axes: 'x'
-                },
-                rotate: {
-                    angle: 270
-                }
-            });
-        });
-    });
-    
-    it('Do not rotate JPG with Orientation 8', function () {
-
-        return doTest(8, '.jpg').then(function (res) {
-
-            expect(res.operations).to.deep.equal({
-                rotate: {
-                    angle: 270
-                }
-            });
-        });
-    });
-
-});
-
+  });
+})
 
 function doTest(index, ext) {
 
-    var inputpath = path.resolve(__dirname, './images/Landscape_' + index + ext);
-    var outputpath = path.resolve(__dirname, './images/Landscape_' + index + '.output' + ext);
+  console.log('doTest');
 
-    var promise = lwipJpegAutorotate.autorotate(inputpath, outputpath);
+  var inputpath = path.resolve(__dirname, './images/Landscape_' + index + ext);
+  var outputpath = path.resolve(__dirname, './images/Landscape_' + index + '.output' + ext);
 
-    promise.then(function (res) {
+  console.log('lwipJpegAutorotate', lwipJpegAutorotate);
 
-        expect(res).to.not.be.undefined;
-        expect(res.operations).to.not.be.undefined;
+  var promise = lwipJpegAutorotate.autorotate(inputpath, outputpath);
 
-    }, function (err) {
-        assert.fail('Unexpected error during auto-rotation.');
-    });
+  promise.then(function (res) {
 
-    promise.done();
+    expect(res).to.not.be.undefined;
+    expect(res.operations).to.not.be.undefined;
 
-    return promise;
+  }, function (err) {
+    assert.fail('Unexpected error during auto-rotation.');
+  });
+
+  promise.done();
+
+  return promise;
 }
